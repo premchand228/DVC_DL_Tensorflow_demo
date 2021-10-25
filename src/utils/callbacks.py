0179bfce7cc9ad7210568d68f014f87abf632264
premchand228 <pream.chand228@gmail.com>
2021-10-25 14:20:51 +0530

    commiting the callbacks

---
+++ src/utils/callbacks.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import os
+import joblib
+import logging
+from src.utils.all_utils import get_timestamp
+
+def create_and_save_tensorboard_callback(callbacks_dir, tensorboard_log_dir):
+    return None
+
+def create_and_save_checkpoint_callback(callbacks_dir, checkpoint_dir):
+    return None
