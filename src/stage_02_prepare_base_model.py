c0361ca053a17204567700f081f18d37208766c8
premchand228 <pream.chand228@gmail.com>
2021-10-25 13:34:38 +0530

    pushing pending changes of class 2

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -1,4 +1,3 @@
-from tensorflow.python.keras.applications.vgg16 import VGG16
 from src.utils.all_utils import read_yaml, create_directory
 from src.utils.models import get_VGG_16_model, prepare_model
 import argparse
@@ -13,18 +12,36 @@ logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=lo
                     filemode="a")
 
 def prepare_base_model(config_path, params_path):
-    config=read_yaml(config_path)
-    params=read_yaml(params_path)
-    artifacts=config["artifacts"]
-    artifacts_dir=artifacts["ARTIFACTS_DIR"]
-    base_model_dir=artifacts["BASE_MODEL_DIR"]
-    base_model_name=artifacts["BASE_MODEL_NAME"]
-    base_model_dir_path=os.path.join(artifacts_dir,base_model_dir)
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+
+    artifacts = config["artifacts"]
+    artifacts_dir = artifacts["ARTIFACTS_DIR"]
+
+    base_model_dir = artifacts["BASE_MODEL_DIR"]
+    base_model_name = artifacts["BASE_MODEL_NAME"]
+
+    base_model_dir_path = os.path.join(artifacts_dir, base_model_dir)
+
     create_directory([base_model_dir_path])
-    base_model_path=os.path.join(base_model_dir_path,base_model_name)
-    model=get_VGG_16_model(input_shape=params["IMAGE_SIZE"], model_path= base_model_path)
-    full_model=prepare_base_model(model,    classes= params["CLASSES"],freeze_all=True ,freeze_till=None,learning_rate=params["LEARNI   NG_RATE"])
-    updated_model_path=os.path.join(base_model_dir,artifacts["UPDATED_BASE_MODEL_NAME"])
+
+    base_model_path = os.path.join(base_model_dir_path, base_model_name)
+
+    model = get_VGG_16_model(input_shape=params["IMAGE_SIZE"], model_path=base_model_path)
+
+    full_model = prepare_model(
+        model,
+        CLASSES=params["CLASSES"],
+        freeze_all=True,
+        freeze_till=None,
+        learning_rate=params["LEARNING_RATE"]
+    )
+
+    update_base_model_path = os.path.join(
+        base_model_dir_path,
+        artifacts["UPDATED_BASE_MODEL_NAME"]
+    )
+
     def _log_model_summary(full_model):
         with io.StringIO() as stream:
             full_model.summary(print_fn=lambda x: stream.write(f"{x}\n"))
@@ -33,9 +50,9 @@ def prepare_base_model(config_path, params_path):
 
     logging.info(f"full model summary: \n{_log_model_summary(full_model)}")
 
-    full_model.save(updated_model_path)
+    full_model.save(update_base_model_path)
     
-    
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
